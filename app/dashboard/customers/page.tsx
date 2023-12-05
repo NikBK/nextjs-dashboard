84bfad3265f396e84a0d285d08cdeb344e5036fd
NikBK <nikhilbalakrishna5@gmail.com>
2023-12-05 12:49:50 +0530

    customers page added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,30 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import Pagination from '@/app/ui/invoices/pagination';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customer Page</p>;
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return <div className='w-full'>
+        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+            <CustomersTable customers={customers} />
+        </Suspense>
+    </div>
 }
